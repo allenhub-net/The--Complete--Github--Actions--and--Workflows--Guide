409518ff1ed593c70cdb5be688fdf4a84a4250c8
michaeldallen <michael@allenhub.net>
2025-07-29 16:54:46 +0000

    cl5-test3

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
-      2, // error level (fail on vi o l ation)
+      2, // error level (fail on vi o l ati on)
       'always',
       ['feat', 'fix'], // ONLY allow 'feat' and 'fix'
     ],
